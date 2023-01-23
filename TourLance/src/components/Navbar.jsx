676eb44779ef6aa001484a472a66f983a067bb0d
DiyRex <dsdissanayaka2002@gmail.com>
2023-01-23 12:22:12 +0530

    bootstrap --added

--- TourLance/src/components/Navbar.jsx
+++ TourLance/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 export default function navbar() {
   return (
-    <div>navbar</div>
+    <div>
+    <Button as="a" variant="primary">
+      Button as link
+    </Button>
+    <Button as="a" variant="success">
+      Button as link
+    </Button>
+  </div>
   )
 }
