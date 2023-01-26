4f5d49bee8e9b04706ca71c342906e0bddbaa983
Dinuka <kpdlakshan@gmail.com>
2023-01-26 14:03:33 +0530

    Visit Sri Lanka

---
+++ TourLance/src/components/VisitSL.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/visitsl.css'
+import Ancient from '../assets/sri-lanka-ancient-ruins-1024x682.jpg'
+import Beach from '../assets/IMG_6106-1000x750.jpg'
+import Safari from '../assets/11.jpg'
+import Hiking from '../assets/cover-image-e1470286833146.jpg'
+import Other from '../assets/db8a210249f3cd9b44dc5cd3416cadca.jpg'
+import Camping from '../assets/camping-soulful-experience.jpg'
+
+export default function VisitSL() {
+  return (
+    <>
+      <div className='ancient'>
+
+      </div>
+      <div className='safari'>
+
+      </div>
+      <div className='Hiking'>
+
+      </div>
+      <div className='Camping'>
+
+      </div>
+      <div className='Other'>
+
+      </div>
+      <div className='Beach'>
+        
+      </div>
+    </>
+  )
+}
