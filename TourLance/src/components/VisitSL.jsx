651c62ee3b01f82c601bdcff2f08aa8699ecb575
Dinuka <kpdlakshan@gmail.com>
2023-01-26 21:52:14 +0530

    tiles

--- TourLance/src/components/VisitSL.jsx
+++ TourLance/src/components/VisitSL.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../styles/visitsl.css'
-import Ancient from '../assets/sri-lanka-ancient-ruins-1024x682.jpg'
-import Beach from '../assets/IMG_6106-1000x750.jpg'
-import Safari from '../assets/11.jpg'
-import Hiking from '../assets/cover-image-e1470286833146.jpg'
-import Other from '../assets/db8a210249f3cd9b44dc5cd3416cadca.jpg'
-import Camping from '../assets/camping-soulful-experience.jpg'
+import Ancient from '../assets/Ancient.png'
+import Beach from '../assets/Beach.png'
+import Safari from '../assets/Safari.png'
+import Hiking from '../assets/Hiking.png'
+import Other from '../assets/other.png'
+import Camping from '../assets/camping.png'
 
 export default function VisitSL() {
   return (
@@ -18,21 +18,22 @@ export default function VisitSL() {
 
       </div>
       <div className='Hiking'>
-      <img src={Hiking} alt="" />
+        <img src={Hiking} alt="" />
 
       </div>
       <div className='Camping'>
-      <img src={Camping} alt="" />
+        <img src={Camping} alt="" />
 
       </div>
       <div className='Other'>
-      <img src={Other} alt="" />
+        <img src={Other} alt="" />
 
       </div>
       <div className='Beach'>
-      <img src={Beach} alt="" />
+        <img src={Beach} alt="" />
 
       </div>
     </>
   )
 }
+
--- TourLance/src/views/home.jsx
+++ TourLance/src/views/home.jsx
@@ -3,6 +3,7 @@ import Footer from '../components/Footer'
 import NavBar from '../components/navbar'
 import Seegiriya from '../assets/seegiriya2.jpg'
 import '../styles/home.css'
+import VisitSL from '../components/VisitSL'
 
 export default function home() {
   return (
@@ -13,6 +14,7 @@ export default function home() {
         <div className="homeimg">
             <img className='main-img' src={Seegiriya} alt="" />
         </div>
+        <VisitSL />
         <Footer />
     </div>
   )
