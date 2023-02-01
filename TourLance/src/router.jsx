b0a887e526f88945ee64994783aea3dcf0c8dfb1
DiyRex <dsdissanayaka2002@gmail.com>
2023-02-01 18:11:41 +0530

    tailwindcss added

--- TourLance/src/router.jsx
+++ TourLance/src/router.jsx
@@ -34,6 +34,10 @@ const router = createBrowserRouter([
         path: '/about',
         element: <AboutUS />,
       },
+      {
+        path: '/test',
+        element: <Dashboard/>,
+      },
     ]
   },
   {
